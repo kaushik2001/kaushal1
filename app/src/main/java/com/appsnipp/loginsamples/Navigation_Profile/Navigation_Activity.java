d332d2b483cd4459cb1c5acadd3dd71f99a9b6c6
kaushik2001 <mokariyakaushik2001@gmail.com>
2019-09-20 21:55:37 +0530

    last work

--- app/src/main/java/com/appsnipp/loginsamples/Navigation_Profile/Navigation_Activity.java
+++ app/src/main/java/com/appsnipp/loginsamples/Navigation_Profile/Navigation_Activity.java
@@ -2,6 +2,7 @@ package com.appsnipp.loginsamples.Navigation_Profile;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -30,6 +31,7 @@ import com.appsnipp.loginsamples.Navigation_Profile.ui.complain.ComplainFragment
 import com.appsnipp.loginsamples.Navigation_Profile.ui.dashboard.DashBoardFragment;
 import com.appsnipp.loginsamples.Navigation_Profile.ui.event.EventFragment;
 import com.appsnipp.loginsamples.Navigation_Profile.ui.noticeboard.NoticeBoardFragment;
+import com.appsnipp.loginsamples.Navigation_Profile.ui.profile.ProfileFragment;
 import com.appsnipp.loginsamples.Navigation_Profile.ui.resource.ResourceFragment;
 import com.appsnipp.loginsamples.Navigation_Profile.ui.document.DocumentFragment;
 import com.appsnipp.loginsamples.Navigation_Profile.ui.members.MembersFragment;
@@ -41,29 +43,83 @@ public class Navigation_Activity extends AppCompatActivity implements  Navigatio
     Fragment fragment;
     FragmentManager manager;
     FragmentTransaction transaction;
+    BottomNavigationView bottomNavigationView;
+    NavigationView navigationView;
     private AppBarConfiguration mAppBarConfiguration;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            int id = item.getItemId();
+            navigationView=findViewById(R.id.nav_view);
+            if (id == R.id.navB_home) {
+
+
+                navigationView.setCheckedItem(R.id.nav_home);
+                fragment = new DashBoardFragment();
+
+
+            } else if (id == R.id.navB_building) {
+                navigationView.setCheckedItem(R.id.nav_building);
+                fragment = new BuildingDetailsFragment();
+
+
+            } else if (id == R.id.navB_profile) {
+                navigationView.setCheckedItem(R.id.nav_profile);
+                fragment = new ProfileFragment();
+
+            }
+            else if (id == R.id.navB_notice) {
+                navigationView.setCheckedItem(R.id.nav_notice);
+                fragment = new NoticeBoardFragment();
+
+            }else if (id == R.id.navB_menu) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+            }
+
+
+            transaction = manager.beginTransaction();
+            transaction.replace(R.id.nav_host_fragment, fragment,"A");
+            transaction.addToBackStack("addA");
+            transaction.commit();
+
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        manager=getSupportFragmentManager();
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//      /*  FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });*/
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+         navigationView =findViewById(R.id.nav_view);
+        bottomNavigationView = findViewById(R.id.nav_view_bottom);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigationView.setSelectedItemId(R.id.navB_home);
+        navigationView=findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(R.id.nav_home);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_account, R.id.nav_member,
-                R.id.nav_election, R.id.nav_document, R.id.nav_resource)
+                R.id.nav_election, R.id.nav_document, R.id.nav_resource,R.id.nav_profile,R.id.navB_menu,R.id.nav_building,R.id.nav_complain,
+        R.id.nav_event,R.id.nav_notice,R.id.nav_visitor,R.id.navB_building,R.id.navB_home,R.id.navB_menu,R.id.navB_notice)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -77,6 +133,9 @@ public class Navigation_Activity extends AppCompatActivity implements  Navigatio
                 drawer.openDrawer(GravityCompat.START);
             }
         });
+
+
+
     }
 
     @Override
@@ -93,11 +152,16 @@ public class Navigation_Activity extends AppCompatActivity implements  Navigatio
                 || super.onSupportNavigateUp();
     }
 
+
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         int id = menuItem.getItemId();
 
         if (id == R.id.nav_home) {
+
+
+
             Toast.makeText(this, "Activity is refresh", Toast.LENGTH_SHORT).show();
             fragment = new DashBoardFragment();
 
@@ -107,13 +171,16 @@ public class Navigation_Activity extends AppCompatActivity implements  Navigatio
 
 
         } else if (id == R.id.nav_member) {
+
             fragment = new MembersFragment();
 
         }
         else if (id == R.id.nav_notice) {
+
             fragment = new NoticeBoardFragment();
 
         }else if (id == R.id.nav_complain) {
+
             fragment = new ComplainFragment();
 
         }else if (id == R.id.nav_visitor) {
@@ -123,6 +190,7 @@ public class Navigation_Activity extends AppCompatActivity implements  Navigatio
             fragment = new ElectionFragment();
 
         } else if (id == R.id.nav_building) {
+
             fragment = new BuildingDetailsFragment();
 
         }else if (id == R.id.nav_document) {
@@ -137,6 +205,15 @@ public class Navigation_Activity extends AppCompatActivity implements  Navigatio
 
 
         }
+        else if (id == R.id.nav_profile) {
+
+            fragment = new ProfileFragment();
+
+
+        }
+
+
+
         transaction = manager.beginTransaction();
         transaction.add(R.id.nav_host_fragment, fragment, "A");
         transaction.addToBackStack("addA");
---
+++ app/src/main/java/com/appsnipp/loginsamples/Navigation_Profile/ui/profile/ProfileFragment.java
@@ -0,0 +1,34 @@
+package com.appsnipp.loginsamples.Navigation_Profile.ui.profile;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.appsnipp.loginsamples.R;
+
+public class ProfileFragment extends Fragment {
+
+    private ProfileViewModel profileViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        profileViewModel =
+                ViewModelProviders.of(this).get(ProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        final TextView textView = root.findViewById(R.id.text_profile);
+        profileViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
